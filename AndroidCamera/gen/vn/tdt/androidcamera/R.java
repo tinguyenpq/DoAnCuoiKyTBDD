60a809a1ee526dd1ca476b883f62e0f4661c37f7
Nguyen Thanh Tien <tinguyenpq@gmail.com>
2015-03-21 21:35:19 +0700

    up

--- AndroidCamera/gen/vn/tdt/androidcamera/R.java
+++ AndroidCamera/gen/vn/tdt/androidcamera/R.java
@@ -171,9 +171,12 @@ public final class R {
         public static final int shooting_selected=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f060002;
+        public static final int action_settings=0x7f060005;
+        public static final int imageView1=0x7f060004;
+        public static final int image_effect=0x7f060003;
         public static final int image_gallery=0x7f060001;
         public static final int image_setting=0x7f060000;
+        public static final int image_shooting=0x7f060002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
