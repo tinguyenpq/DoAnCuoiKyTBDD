42a79f4e8ee7f9218c493bf27e6dacd4c7d9252a
Nguyen Thanh Tien <tinguyenpq@gmail.com>
2015-04-20 17:50:13 +0700

    tab

--- AndroidCamera/gen/vn/tdt/androidcamera/R.java
+++ AndroidCamera/gen/vn/tdt/androidcamera/R.java
@@ -170,15 +170,16 @@ public final class R {
         public static final int eff01=0x7f020003;
         public static final int effect_camera=0x7f020004;
         public static final int ic_launcher=0x7f020005;
-        public static final int image_gallery=0x7f020006;
-        public static final int infomation=0x7f020007;
-        public static final int my_album_normal_64=0x7f020008;
-        public static final int my_album_selected_64=0x7f020009;
-        public static final int other_album_normal_64=0x7f02000a;
-        public static final int other_album_selected_64=0x7f02000b;
-        public static final int setting=0x7f02000c;
-        public static final int shooting_normal=0x7f02000d;
-        public static final int shooting_selected=0x7f02000e;
+        public static final int image_gallery_normal_64=0x7f020006;
+        public static final int image_gallery_selected_64=0x7f020007;
+        public static final int infomation=0x7f020008;
+        public static final int my_album_normal_64=0x7f020009;
+        public static final int my_album_selected_64=0x7f02000a;
+        public static final int other_album_normal_64=0x7f02000b;
+        public static final int other_album_selected_64=0x7f02000c;
+        public static final int setting=0x7f02000d;
+        public static final int shooting_normal=0x7f02000e;
+        public static final int shooting_selected=0x7f02000f;
     }
     public static final class id {
         public static final int action_settings=0x7f060016;
--- AndroidCamera/src/vn/tdt/androidcamera/tabhost/GalleryActivity.java
+++ AndroidCamera/src/vn/tdt/androidcamera/tabhost/GalleryActivity.java
@@ -1,8 +1,9 @@
 package vn.tdt.androidcamera.tabhost;
 
 import vn.tdt.androidcamera.R;
-import vn.tdt.androidcamera.R.layout;
+
 import android.app.Activity;
+import android.app.LocalActivityManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TabHost;
@@ -17,9 +18,11 @@ public class GalleryActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gallery);
 
-		mTabHost = (TabHost) findViewById(R.id.tab_host_gallery);
-		mTabHost.setup();
+		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
 
+		mTabHost.setup();
+		
+		
 		// Tab for My album tab
 		TabSpec myAlbumSpec = mTabHost.newTabSpec("MyAlbum");
 		// setting Title and Icon for the Tab
@@ -39,6 +42,9 @@ public class GalleryActivity extends Activity {
 		// Adding all TabSpec to TabHost
 		mTabHost.addTab(myAlbumSpec); // Adding My album tab
 		mTabHost.addTab(otherAlbumSpec); // Adding Other album tab
+
+		// set Windows tab as defaualt (zero based)
+		mTabHost.setCurrentTab(1);
 	}
 
 }
